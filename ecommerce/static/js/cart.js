984239ad864e200a708c4d3cf3d80470f2e1df52
Jed Peek <jedpeek@jeds-mbp.home>
2020-12-08 22:15:11 -0800

    updated cart functionality

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,36 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(i=0; i < updateBtns.length; i++){
+  updateBtns[i].addEventListener('click', function(){
+    var productId = this.dataset.product
+    var action = this.dataset.action
+    console.log('productId:', productId, 'Action:', action)
+
+    console.log('USER:', user)
+    if(user == "AnonymousUser"){
+      console.log("Not logged in")
+    }else{
+      updateUserOrder(productId, action)
+    }
+
+  })
+}
+
+function updateUserOrder(productId, action){
+      console.log("HI"),
+      var url = '/update_item/'
+      fetch(url, {
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+      })
+      .then((response) => response.json())
+      .then((data)=> console.log(data), location.reload())
+      // .catch(err => {
+      //     console.log("Error Reading data " + err);
+      //   });
+}
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 def store(request):
     products =Product.objects.all()
     context = {'products':products}
     return render(request, 'store/store.html', context)
+
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -15,6 +18,7 @@ def cart(request):
         order = {'get_cart_total':0, "get_cart_items":0}
     context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
+
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -25,3 +29,28 @@ def checkout(request):
         order = {'get_cart_total':0, "get_cart_items":0}
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print("Action:", action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == "add":
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == "remove":
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse("Item was added", safe=False)
